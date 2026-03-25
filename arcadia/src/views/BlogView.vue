d1eeaa28a2a5052971a2d131231104e80dde840d
Saurabh Kulkarni <tobeaprogrammer@users.noreply.github.com>
2026-03-25 19:46:25 +0530

    updates

--- arcadia/src/views/BlogView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="h-1/2 w-full">
-        <h3 class="text-center text-2xl font-semibold mb-2 text-yellow-600">Legal Awareness</h3>
-        <div class="flex items-center justify-center mt-10">
-            <img src="../assets/comming-soon.svg" alt="comming soon"  class="w-100 h-100">
-        </div>
-    </div>
-</template>
